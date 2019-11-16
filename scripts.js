f572447c8abed58ac2b4046813488ae9af426d40
Victor gao <33037196+Aaerialys@users.noreply.github.com>
2019-11-16 02:32:12 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+window.onclick=function(event) {
+	
+	if(event.target.className=="popup")
+		event.target.style.display="none";
+}
+
+var clickbox=document.getElementsByClassName("box41");
+for (var i=0;i<clickbox.length;i++){
+	clickbox[i].onclick=function(){
+		document.getElementById("popup-"+this.id).style.display="block";
+	}
+}
